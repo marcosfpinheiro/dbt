8e2c0b6a2257a0dfe6831b970cd2700c9c7f0ced
Marcos Pinheiro <pinheiro.tech@gmail.com>
2024-10-08 13:09:05 +0000

    add new mart credit card user per country

---
+++ models/marts/credit_card_usage_per_country.sql
@@ -0,0 +1,16 @@
+with 
+cleansed_user as
+(
+    select * from {{ ref('clean_user')}}
+),
+cleansed_payments as(
+    select * from {{ ref('cleansed_payments')}}
+),
+credit_card_usage_per_country as 
+(
+    select ps.country, ps.currency, ps.credit_card_type, count(*) as amount
+    from clean_user as pu
+    inner join cleansed_payments as ps using(user_id)
+    group by ps.country, ps.currency, ps.credit_card_type
+)
+select * from credit_card_usage_per_country
